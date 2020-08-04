370d1253c73828dd399d7e76e9af34c961ddfd5a
pnemec <petr.nemec@transen.sk>
2020-08-04 13:53:13 +0200

    properties

--- src/main/java/germinoci/springframework/config/PropertyConfig.java
+++ src/main/java/germinoci/springframework/config/PropertyConfig.java
@@ -1,15 +1,31 @@
 package germinoci.springframework.config;
 
 import germinoci.springframework.examplebeans.FakeDataSource;
+import germinoci.springframework.examplebeans.FakeJmsBroker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+// alternativa
+@PropertySources(
+    {
+        @PropertySource("classpath:datasource.properties"),
+        @PropertySource("classpath:jms.properties")
+    }
+
+)
 public class PropertyConfig {
 
+    @Autowired
+    Environment env;
+
+    //datasource
     @Value("${germinoci.username}")
     String user;
     @Value("${germinoci.password}")
@@ -17,18 +33,42 @@ public class PropertyConfig {
     @Value("${germinoci.dburl}")
     String url;
 
+    //jms
+    @Value("${germinoci.jsm.username}")
+    String jmsUser;
+    @Value("${germinoci.jsm.password}")
+    String jmsPassword;
+    @Value("${germinoci.jsm.dburl}")
+    String jmsUrl;
+
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
-        fakeDataSource.setUser(user);
+        // fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("GERMINOCIx_USERNAME"));// idea overide user environment variables
         return fakeDataSource;
     }
 
-//    @Bean
+    @Bean
+    public FakeJmsBroker fakeJmsBroker() {
+        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+        fakeJmsBroker.setPassword(jmsPassword);
+        fakeJmsBroker.setUrl(jmsUrl);
+        fakeJmsBroker.setUser(jmsUser);
+        return fakeJmsBroker;
+    }
+
+//    @Bean // s5 dont need
 //    public static PropertySourcesPlaceholderConfigurer properties() {
 //        return new PropertySourcesPlaceholderConfigurer();
 //    }
 
+//    @Bean // or ... no //@PropertySource("classpath:datasource.properties")
+//    public static PropertySourcesPlaceholderConfigurer properties() {
+//        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+//        configurer.setLocation(new ClassPathResource("datasource.properties"));
+//        return configurer;
+//    }
 }
